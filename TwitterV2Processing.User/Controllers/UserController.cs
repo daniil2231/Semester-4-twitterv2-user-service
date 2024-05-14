ea6191a1208e05af9c58a7163ac386cced53e288
Dandi <476545@student.fontys.nl>
2024-05-14 13:05:04 +0200

    fix: import errors

--- TwitterV2Processing.User/Controllers/UserController.cs
+++ TwitterV2Processing.User/Controllers/UserController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
 using MongoDB.Driver;
-using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using TwitterV2Processing.User.Business;
